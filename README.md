b2700047d70a391d62ef13b9e765031e41ebfecc
Yar Reech <yarreech@gmail.com>
2020-10-03 00:23:37 -0600

    Completed memory game
    Completed the memory game project

---
+++ README.md
@@ -0,0 +1,10 @@
+This memory game is project 1 of The 'Learn Javascript by Building 7 games course' from FreeCodeCamp's Youtube page.
+
+Link to video course: https://www.youtube.com/watch?v=lhNdUVh3qCc&t=11s
+
+
+Course is taught by Ania Kubow 
+
+Youtube: https://www.youtube.com/channel/UC5DNytAJ6_FISueUfzZCVsw
+
+github: https://github.com/kubowania/memory-game
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
 	// card options
 	const cardArray = [
 
@@ -46,9 +46,71 @@ document.addEventListener("DOMContentLoaded", () =>{
 		name: "pizza", 
 		img: "images/pizza.png"
 	},
+	{
+		name: "pizza", 
+		img: "images/pizza.png"
+	}
+]
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const grid = document.querySelector(".grid");
+const resultDisplay = document.querySelector("#result");
+var cardsChosen = [];
+var cardsChosenId = [];
+var cardsWon = [];
+
+//create your board
+function createBoard(){
+	for( let i = 0; i < cardArray.length; i++){
+		var card = document.createElement("img");
+		card.setAttribute("src", "images/blank.png");
+		card.setAttribute("data-id", i);
+		card.addEventListener("click", flipCard);
+		grid.appendChild(card);
+	}
+}
+
+//check for matches
+function checkForMatch(){
+	var cards = document.querySelectorAll("img");
+	const optionOneId = cardsChosenId[0];
+	const optionTwoId = cardsChosenId[1];
 
 
+	if(cardsChosen[0] === cardsChosen[1]){
+		alert("You have found a match!");
+		cards[optionOneId].setAttribute("src", "images/white.png");
+		cards[optionTwoId].setAttribute("src", "images/white.png");
+		cards[optionOneId].removeEventListener("click", flipCard);
+		cards[optionTwoId].removeEventListener("click", flipCard);
+		cardsWon.push(cardsChosen);
+	}
+	else {
+		alert("Sorry not a match try again")
+		cards[optionOneId].setAttribute("src", "images/blank.png");
+		cards[optionTwoId].setAttribute("src", "images/blank.png");
 
+	}
+	cardsChosen = [];
+	cardsChosenId = [];
+	resultDisplay.textContent = cardsWon.length;
+	if (cardsWon.length === cardsArray.length / 2) {
+		resultDisplay.textContent = "Congratulations! You found all the Matches!"
+	}
+}
 
-	]
-})
+//flip your card
+function flipCard(){
+	var cardId = this.getAttribute("data-id");
+	cardsChosen.push(cardArray[cardId].name);
+	cardsChosenId.push(cardId);
+	this.setAttribute("src", cardArray[cardId].img);
+	if (cardsChosen.length === 2) {
+		setTimeout(checkForMatch, 500);
+	}
+}
+
+createBoard();
+
+});
